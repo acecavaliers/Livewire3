a9fe4bb04bd74d43f1c32c092b9639556ca1947b
acecavaliers <aceofkagaw@gmail.com>
2024-03-13 16:55:10 +0800

    Customer alert

--- resources/views/livewire/customers/index.blade.php
+++ resources/views/livewire/customers/index.blade.php
@@ -2,9 +2,22 @@
 
     <div class="col-md-8 mb-2">
         @if(session()->has('success'))
-            <div class="alert alert-success" role="alert">
+        <div id="alert-1" class="flex items-center p-4 mb-4 text-blue-50 rounded-lg bg-blue-900" role="alert">
+            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div class="ms-3 text-sm font-medium">
                 {{ session()->get('success') }}
+              {{-- A simple info alert with an <a href="#" class="font-semibold underline hover:no-underline">example link</a>. Give it a click if you like. --}}
             </div>
+              <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#alert-1" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+            </button>
+        </div>
         @endif
         @if(session()->has('error'))
             <div class="alert alert-danger" role="alert">
@@ -144,6 +157,7 @@
                         </thead>
                         <tbody class="divide-y divide-cool-gray-200 ">
                             @forelse ($customers as $customer)
+                            {{-- <tr wiere:key="{{ $customer->id }}" wire:loading.class="opacity-50"> --}}
                                 <tr wiere:key="{{ $customer->id }}">
                                     <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                         {{ $customer->name }}
