21f7e9883b62d558bd29244dd7673dccb1d3e416
acecavaliers <aceofkagaw@gmail.com>
2024-03-16 14:37:25 +0800

    ROUTE FX

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,7 @@ Route::get('/', function () {
 })->name('home');
 
 
-Route::get('/customer',Index::class)->name('customer.index');
-// Route::get('/customer/create',Create::class)->name('customer.create');
-// Route::get('/customer/edit',Edit::class)->name('customer.edit');
 
-Route::get('/product',ProductsIndex::class)->name('product.index');
-
-
-Route::get('/supplier',SupplierIndex::class)->name('supplier.index');
 
 Route::middleware([
     'auth:sanctum',
@@ -41,4 +34,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/customer',Index::class)->name('customer.index');
+    // Route::get('/customer/create',Create::class)->name('customer.create');
+    // Route::get('/customer/edit',Edit::class)->name('customer.edit');
+
+    Route::get('/product',ProductsIndex::class)->name('product.index');
+
+
+    Route::get('/supplier',SupplierIndex::class)->name('supplier.index');
 });
